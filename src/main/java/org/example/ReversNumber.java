5e7c8dfe257b52b905f3ab887cc4575c69def1a1
shindeSuraj <shindet985@gmail.com>
2024-11-10 17:15:35 +0530

    reverse number updated

--- src/main/java/org/example/ReversNumber.java
+++ src/main/java/org/example/ReversNumber.java
@@ -6,12 +6,12 @@ public class ReversNumber {
 
         int number=2341;
         int revers=0;
-
-        StringBuffer stringBuffer=new StringBuffer(Integer.toString(number));
-        stringBuffer.reverse();
-        revers = Integer.parseInt(stringBuffer.toString());
-
-        System.out.println(revers);
+        int number=2341;
+        int revers=0;
+        int number=2341;
+        int revers=0;
+        int number=2341;
+        int revers=0;
 
 
     }
